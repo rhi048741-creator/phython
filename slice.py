d3d11e472257228ac479034500a8122569c750e9
rhi048741-creator <rhi048741@gmail.com>
2025-12-12 11:56:28 +0530

    Add files via upload

---
+++ slice.py
@@ -0,0 +1,5 @@
+word='CITIZEN'
+word1=word[0:7]
+word2=word[7:0]
+print(word1)
+print(word2)
---
+++ sliceing.py
@@ -0,0 +1,5 @@
+word='RUNFORUNITY'
+word1=word[-7:-1]#letter from index -7,-6,-5,-4,-3,-2 will be displayed
+word2=word[-5:-2]#letter -5,-4,-3,-2 will be display
+print(word1)
+print(word2)
