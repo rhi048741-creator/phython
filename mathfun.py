2c923863ea899461429847784d6bf60cd8d70d45
rhi048741-creator <rhi048741@gmail.com>
2025-12-12 16:28:43 +0530

    Add files via upload

---
+++ mathfun.py
@@ -0,0 +1,7 @@
+def CBIC(x,y):#name of the function is cbic and and argument
+    k=2*x**3+2*y**3#body of the function
+    return k#statement to return computed result
+a=int(input("enter a number"))#taking integer type of input
+b=int(input("enter a number"))
+print(CBIC(a,b))#function call inside print
+
